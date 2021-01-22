5faca3463b866c777391f1401aaa2838d83ac9af
tamago324 <tamago_pad@yahoo.co.jp>
2021-01-23 00:44:50 +0900

    style: Simplified the implementation

--- lua/lir/mark/actions.lua
+++ lua/lir/mark/actions.lua
@@ -6,23 +6,8 @@ local a = vim.api
 -----------------------------
 -- Private
 -----------------------------
-local MARK_TEXT = '*'
-local SPACES = ' '
-
-local function get_lnum_start_end()
-  -- TODO: Support visual mode
-  -- local mode = a.nvim_get_mode()
-  --
-  -- if mode == 'v' or mode == 'V' then
-  --   -- clear visual mode
-  --   local key = nvim_replace_termcodes('<Esc>', true, true, true)
-  --   a.nvim_feedkeys(key, 'n', true)
-  --   return {vim.fn.line("'<"), vim.fn.line("'>")}
-  -- end
-
-  local lnum = vim.fn.line('.')
-  return {lnum, lnum}
-end
+local MARK_TEXT = '* '
+local SPACES = '  '
 
 local function clear_marks()
   local col = config.values.hide_cursor and 0 or 1
@@ -33,6 +18,15 @@ local function clear_marks()
   end
 end
 
+local function exec_in_modifiable(func)
+
+  a.nvim_buf_set_option(0, 'modifiable', true)
+  local result = func()
+  a.nvim_buf_set_option(0, 'modifiable', false)
+
+  return result
+end
+
 -----------------------------
 -- Export
 -----------------------------
@@ -40,25 +34,20 @@ local M = {}
 
 
 M.mark = function (context)
-  a.nvim_buf_set_option(0, 'modifiable', true)
-  local col = config.values.hide_cursor and 0 or 1
-
-  if #context:marked_items() > 0 then
-    -- 一旦リセット
-    clear_marks()
-  end
-
-  local start_lnum, end_lnum = unpack(get_lnum_start_end())
-  for i = 1, #context.files do
-    if start_lnum <= i and i <= end_lnum then
-      context.files[i].marked = true
+  return exec_in_modifiable(function()
+    if #context:marked_items() > 0 then
+      -- 一旦リセット
+      clear_marks()
     end
 
-    local text = context.files[i].marked and MARK_TEXT or SPACES
-    a.nvim_buf_set_text(0, i-1, col, i-1, col, {text})
-  end
+    context:current().marked = true
 
-  a.nvim_buf_set_option(0, 'modifiable', false)
+    for i = 1, #context.files do
+      local col = config.values.hide_cursor and 0 or 1
+      local text = context.files[i].marked and MARK_TEXT or SPACES
+      a.nvim_buf_set_text(0, i-1, col, i-1, col, {text})
+    end
+  end)
 end
 
 
@@ -67,28 +56,31 @@ M.unmark = function (context)
     return
   end
 
-  a.nvim_buf_set_option(0, 'modifiable', true)
+  return exec_in_modifiable(function()
+    context:current().marked = false
 
-  local col = config.values.hide_cursor and 0 or 1
-  local end_col = col + string.len(MARK_TEXT)
-
-  local start_lnum, end_lnum = unpack(get_lnum_start_end())
-  for i = start_lnum, end_lnum do
-    if context.files[i].marked then
-      context.files[i].marked = false
+    if #context:marked_items() == 0 then
+      clear_marks()
+      return
     end
-  end
 
-  if #context:marked_items() == 0 then
-    clear_marks()
-  else
+    local col = config.values.hide_cursor and 0 or 1
+    local end_col = col + string.len(MARK_TEXT)
+
     for i = 1, #context.files do
       local text = context.files[i].marked and MARK_TEXT or SPACES
       a.nvim_buf_set_text(0, i-1, col, i-1, end_col, {text})
     end
-  end
+  end)
+end
 
-  a.nvim_buf_set_option(0, 'modifiable', false)
+
+M.toggle_mark = function(context)
+  if context:current().marked then
+    M.unmark(context)
+  else
+    M.mark(context)
+  end
 end
 
 
