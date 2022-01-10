ca02a1031a2393ec6bf8967657daf06e4d402523
tamago324 <tamago_pad@yahoo.co.jp>
2022-01-11 07:20:51 +0900

    chore: Changed to not show the empty directory icon

--- lua/lir.lua
+++ lua/lir.lua
@@ -169,7 +169,7 @@ end
 ---@param devicon_enable boolean
 local function set_nocontent_text(devicon_enable)
   -- From vim-clap
-  local text = string.format(" %s Directory is empty", (devicon_enable and "" or " "))
+  local text = " Directory is empty"
   a.nvim_buf_set_virtual_text(0, -1, 0, { { text, "LirEmptyDirText" } }, {})
 end
 
