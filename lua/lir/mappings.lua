3752eed809ca43a088a2da51cced4a4228c3352d
tamago324 <tamago_pad@yahoo.co.jp>
2022-09-15 07:25:26 +0900

    Use vim.keymap.set()

--- lua/lir/mappings.lua
+++ lua/lir/mappings.lua
@@ -11,12 +11,6 @@ local a = vim.api
 -- { bufnr = { esc_key = function } }
 local buf_keymap = {}
 
---- escape_keymap
-local function escape_keymap(key)
-  -- Prepend with a letter so it can be used as a dictionary key
-  return "k" .. key:gsub(".", string.byte)
-end
-
 -----------------------------
 -- Export
 -----------------------------
@@ -31,33 +25,15 @@ function M.apply_mappings(mappings)
   end
   local bufnr = a.nvim_get_current_buf()
   local options = {}
-  options.noremap = true
-  options.silent = true
-  options.nowait = true
 
   for lhs, rhs in pairs(mappings) do
-    local escaped = escape_keymap(lhs)
-    local key_mapping
-    -- cleanup
-    if not buf_keymap[bufnr] then
-      buf_keymap[bufnr] = {}
-      a.nvim_buf_attach(bufnr, false, {
-        on_detach = function()
-          buf_keymap[bufnr] = nil
-        end,
-      })
-    end
-    buf_keymap[bufnr][escaped] = rhs
-    key_mapping = ([[<Cmd>lua require"lir.mappings".execute_keymap(%d, "%s")<CR>]]):format(bufnr, escaped)
-    a.nvim_buf_set_keymap(bufnr, "n", lhs, key_mapping, options)
+    vim.keymap.set('n', lhs, rhs, {
+      buffer = bufnr,
+      noremap = true,
+      silent = true,
+      nowait = true
+    })
   end
-end
-
----@param bufnr number
----@param escaped string
-function M.execute_keymap(bufnr, escaped)
-  local func = buf_keymap[bufnr][escaped]
-  func()
 end
 
 return M
